922ea10958ada5a5d4f3eb55134d656e717815cc
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-05-21 15:23:09 -0400

    update query 7 & 8

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE TABLE crouton.host_activity_reduced (
   host VARCHAR,
   metric_name VARCHAR,
   metric_array ARRAY(INTEGER),
-  month_start VARCHAR
+  month_start_date VARCHAR
   ) WITH
   (
     FORMAT = 'PARQUET',
-    partitioning = ARRAY['metric_name','month_start']
+    partitioning = ARRAY['metric_name','month_start_date']
   )
--- submission/query_8.sql
+++ submission/query_8.sql
@@ -1,30 +1,36 @@
-INSERT INTO crouton.host_activity_reduced
-WITH yesterday AS (
+INSERT INTO
+  crouton.host_activity_reduced
+WITH
+  yesterday AS (
     SELECT
-        *
+      *
     FROM
-        crouton.host_activity_reduced
+      crouton.host_activity_reduced
     WHERE
-        month_start = '2022-08-01'
-),
-today AS (
+      month_start_date = '2023-08-01'
+  ),
+  today AS (
     SELECT
-        *
+      *
     FROM
-        crouton.daily_web_metrics
+      crouton.daily_web_metrics
     WHERE
-        date = DATE('2022-08-02')
-)
+      DATE = DATE('2023-08-03')
+  )
 SELECT
-    COALESCE(t.host, y.host) AS host, 
-    COALESCE(t.metric_name, y.metric_name) AS metric_name,
-    COALESCE(y.metric_array, REPEAT(NULL, CAST(DATE_DIFF('day', DATE('2022-08-01'), t.date) AS INTEGER))) || ARRAY[t.metric_value] AS metric_array,
-    '2022-08-01' AS month_start
+  COALESCE(t.host, y.host) AS host,
+  COALESCE(t.metric_name, y.metric_name) AS metric_name,
+  COALESCE(
+    y.metric_array,
+    REPEAT(
+      NULL,
+      CAST(
+        DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+      )
+    )
+  ) || ARRAY[t.metric_value] AS metric_array,
+  '2023-08-01' AS month_start_date
 FROM
-    today t
-    FULL OUTER JOIN yesterday y ON t.host = y.host
-    AND t.metric_name = y.metric_name
-WHERE
-    CARDINALITY(
-        COALESCE(y.metric_array, ARRAY[]) || ARRAY[t.metric_value]
-    ) = 1
+  today t
+  FULL OUTER JOIN yesterday y ON t.host = y.host
+  AND t.metric_name = y.metric_name
